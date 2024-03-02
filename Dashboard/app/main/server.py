01072e7ffcdd8e17ac57abd2ace92ca1579c2e6b
mooon53 <katherinrad2409@gmail.com>
2024-03-02 14:48:52 +0100

    responsive pages

--- Dashboard/app/main/server.py
+++ Dashboard/app/main/server.py
@@ -1,12 +1,5 @@
 from dash import Dash
 
-app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
+app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True, meta_tags=[{'name': 'viewport',
+                      'content': 'width=device-width, initial-scale=1.0'}])
 server = app.server
-#
-# # meta_tags are required for the app layout to be mobile responsive
-# app = Dash(__name__, suppress_callback_exceptions=True,
-#            meta_tags=[{'name': 'viewport',
-#                        'content': 'width=device-width, initial-scale=1.0'}])
-#
-#
-# server = app.server
