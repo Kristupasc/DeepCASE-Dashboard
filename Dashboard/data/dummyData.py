b70b0977de152d222a6fbbbe52aee4683ce07434
s2689669 <k.cepelis@student.utwente.nl>
2024-03-04 11:36:49 +0100

    Made the risk label dependent on the weight

--- Dashboard/data/dummyData.py
+++ Dashboard/data/dummyData.py
@@ -19,13 +19,32 @@ import pandas as pd
 # df_clusters = pd.DataFrame(clusters_data)
 # print(df_clusters)
 
+def choose_risk(weight):
+    # A weight can be between 0 and 100. The higher the weight, the higher the risk
+    if weight == 0:
+        return "Unlabeled"
+    elif weight < 10:
+        return "Info"
+    elif weight < 30:
+        return "Low"
+    elif weight < 50:
+        return "Medium"
+    elif weight < 70:
+        return "High"
+    elif weight < 90:
+        return "Suspicious"
+    else:
+        return "Attack"
+
+weights = np.random.randint(0, 11, 100)
+risk_labels = [choose_risk(weight) for weight in weights]
 
 clusters_data = {
     'Time': pd.date_range(start='2023-01-01',periods=100, freq='D'),
     'Type': ['A', 'B', 'C', 'D', 'E'] * 20,
     'Source': ['X', 'Y', 'Z', 'W', 'V'] * 20,
-    'Weight': np.random.randint(0, 11, 100),
-    'Risk Label': np.random.choice(['Info', 'Low', 'Medium', 'High', 'Attack', 'Suspicious', 'Unlabeled'], 100)
+    'Weight': weights,
+    'Risk Label': risk_labels
 }
 df_clusters = pd.DataFrame(clusters_data)
 # df_clusters.to_csv("df_clusters3.csv", index=False)
