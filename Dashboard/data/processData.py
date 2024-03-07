775a77369331bbb02b267cd5db389aef862578e5
Kristupasc <k.cepelis@gmail.com>
2024-03-07 11:01:24 +0100

    processData.py now creates sequences as .csv

---
+++ Dashboard/data/processData.py
@@ -0,0 +1,63 @@
+import os
+from deepcase.preprocessing.preprocessor import Preprocessor
+import torch
+
+"""
+This file is used to process the data from the database and return it in a format that can be used by the front end.
+Specifically, here we run the DeepCASE algorithm on the uploaded data and return the specific steps
+"""
+
+def create_sequences(file):
+    """
+    Create sequences from the uploaded file
+    :param file: the uploaded file
+    :return: the sequences
+    """
+
+    # the command to run it is:
+    # python3 -m deepcase sequence --csv alerts.csv --save-sequences sequences.save
+
+    # Create preprocessor
+    preprocessor = Preprocessor(
+        length=10,  # 10 events in context
+        timeout=86400,  # Ignore events older than 1 day (60*60*24 = 86400 seconds)
+    )
+
+    # Load data from file
+    context, events, labels, mapping = preprocessor.csv(
+        file, verbose=True
+    )
+
+    # Save sequences if necessary
+    with open("sequences.save", 'wb') as outfile:
+        result = {
+            "events": events,
+            "context": context,
+            "labels": labels,
+            "mapping": mapping,
+        }
+        torch.save(result, outfile)
+
+    print(result)
+    # convert context to list
+    context = context.tolist()
+    # convert events to list
+    events = events.tolist()
+    # convert labels to list
+    labels = labels.tolist()
+
+    with open("sequences.csv", "w") as result_file:
+        result_file.write("Context,Event,Label\n")
+        for i in range(len(context)):
+            result_file.write(f"{context[i]},{events[i]},{labels[i]}\n")
+
+    # add the mapping to mapping.csv
+    with open("mapping.csv", "w") as mapping_file:
+        mapping_file.write("Key,Value\n")
+        for key, value in mapping.items():
+            mapping_file.write(f"{key},{value}\n")
+
+
+
+
+create_sequences("alerts.csv")
