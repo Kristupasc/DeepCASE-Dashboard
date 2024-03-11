4799be8f40e215d69938c76b47be44bc24e3e7c4
Kristupasc <k.cepelis@gmail.com>
2024-03-11 15:37:21 +0100

    Made processData.py use deepcase library instead of local implementation

--- Dashboard/data/processData.py
+++ Dashboard/data/processData.py
@@ -6,9 +6,6 @@ This file is used to process the data from the database and return it in a forma
 Specifically, here we run the DeepCASE algorithm on the uploaded data and return the specific steps
 """
 
-main_file_path = '../../DeepCase/deepcase/__main__.py'
-
-
 def create_sequences(file):
     """
     Create sequences from the uploaded file
@@ -19,7 +16,7 @@ def create_sequences(file):
     # python3 -m deepcase sequence --csv alerts.csv --save-sequences sequences.save
 
     arguments = ['sequence', '--csv', file, '--save-sequences', 'sequences.save']
-    command = ['python3', main_file_path] + arguments
+    command = ['python3', '-m', 'deepcase'] + arguments
     subprocess.run(command, check=True)
 
     with open('sequences.save', 'rb') as infile:
@@ -54,7 +51,7 @@ def train_context_builder():
     # the command to run it is:
     # python3 -m deepcase train --load-sequences sequences.save --save-builder builder.save
     arguments = ['train', '--load-sequences', 'sequences.save', '--save-builder', 'builder.save']
-    command = ['python3', main_file_path] + arguments
+    command = ['python3', '-m', 'deepcase'] + arguments
     subprocess.run(command, check=True)
     # TODO: the result is an ordered dict of weights so I don't know how do we convert it to .csv to make it make sense
 
@@ -69,9 +66,8 @@ def create_interpreter_clusters():
     #                                                   --save-interpreter interpreter.save --save-clusters clusters.csv
 
     arguments = ['cluster', '--load-sequences', 'sequences.save', '--load-builder', 'builder.save',
-                 '--save-interpreter',
-                 'interpreter.save', '--save-clusters', 'clusters.csv']
-    command = ['python3', main_file_path] + arguments
+                 '--save-interpreter', 'interpreter.save', '--save-clusters', 'clusters.csv']
+    command = ['python3', '-m', 'deepcase'] + arguments
     subprocess.run(command, check=True)
 
 
@@ -86,13 +82,13 @@ def manual_mode():
     arguments = ['manual', '--load-sequences', 'sequences.save', '--load-builder', 'builder.save',
                  '--load-interpreter', 'interpreter.save', '--load-clusters', 'clusters.csv', '--save-interpreter',
                  'interpreter_fitted.save']
-    command = ['python3', main_file_path] + arguments
+    command = ['python3', '-m', 'deepcase'] + arguments
     subprocess.run(command, check=True)
 
     #TODO: open the interpreter_fitted.save and get the clusters and scores to store in the .csv file
 
 
-def automatic_mode(printing = False):
+def automatic_mode(printing=False):
     """
     Run the deepcase algorithm in automatic mode
     """
@@ -102,7 +98,7 @@ def automatic_mode(printing = False):
     #                                      --load-interpreter interpreter_fitted.save --save-prediction prediction.csv
     arguments = ['automatic', '--load-sequences', 'sequences.save', '--load-builder', 'builder.save',
                  '--load-interpreter', 'interpreter.save', '--save-prediction', 'prediction.csv']
-    command = ['python3', main_file_path] + arguments
+    command = ['python3', '-m', 'deepcase'] + arguments
     subprocess.run(command, check=True)
     #TODO: this is for some reason, not the result we get if we run all this in the command line. We need to check why
 
