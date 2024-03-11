071a2726d0b0bbf6e6923993fa801ad9844d2037
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-03-11 19:23:29 +0100

    Set-up filter.

--- Dashboard/app/main/pages/loaddata.py
+++ Dashboard/app/main/pages/loaddata.py
@@ -7,3 +7,10 @@ def concatTwoFiles(filepath1, filepath2):
     df1 = read_data(filepath1)
     df2 = read_data(filepath2)
     return pd.concat([df1, df2], axis=1, join='inner')
+def formatSequence():
+    df = concatTwoFiles('../../data/sequences.csv', '../../data/alerts.csv')
+    df = df[['timestamp', 'machine', 'Event', 'label', 'Context']]
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+    df['label'] = pd.to_numeric(df['label'])
+    df['Event'] = pd.to_numeric(df['Event'])
+    return df
--- Dashboard/app/main/pages/sequences.py
+++ Dashboard/app/main/pages/sequences.py
@@ -4,14 +4,21 @@ import pandas as pd
 import Dashboard.app.main.pages.loaddata as load
 
 dash.register_page(__name__, path="/sequences", name="Sequences", title="Sequences", order=5)
+df = load.formatSequence()
 
 layout = html.Div([
     html.H1('Sequence'),
-
+dash_table.DataTable(
+    columns = [
+        {'name': 'timestamp', 'id':'timestamp', 'type': 'datetime'},
+{'name': 'machine', 'id':'machine', 'type': 'any'},
+{'name': 'Event', 'id':'Event', 'type': 'numeric'},
+{'name': 'label', 'id':'label', 'type': 'numeric'},
+{'name': 'Context', 'id':'Context', 'type': 'any'}
+    ],
+    data=df.to_dict('records'), page_size = 10)
+# https://dash.plotly.com/datatable/filtering
 ])
-df = load.concatTwoFiles('../../data/sequences.csv','../../data/alerts.csv')
-dash_table.DataTable(data=df.to_dict('records'), page_size = 10)
-
 
 
 
