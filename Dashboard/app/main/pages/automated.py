84971f0355cbc6db6e9ee95e1ff6d911e9ec64da
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-03-14 14:23:18 +0100

    Original id also for automated

--- Dashboard/app/main/pages/automated.py
+++ Dashboard/app/main/pages/automated.py
@@ -1,24 +1,49 @@
 import dash
-from dash import html, dash_table
+from dash import html, dash_table, dcc
 import pandas as pd
 import Dashboard.app.main.recources.loaddata as load
 import Dashboard.app.main.recources.style as style
 dash.register_page(__name__, path="/semi-automatic", name="Semi-automatic", title="Semi-automatic", order=2)
 
-df = load.formatSequence()
 
+id_str = "_sa"
+df = load.formatSequenceCluster(0, id_str)
+set_cluster = load.possible_clusters()
 layout = html.Div([
-    html.H1('Semi-automatic Analysis')
-    # ,
+    html.H1('Semi-automatic'),
+    dcc.Dropdown(
+            id="filter_dropdown"+ id_str,
+            options=[{"label": st, "value": st} for st in set_cluster],
+            placeholder="-Select a State-",
+            multi=False,
+            value=list(set_cluster)
+        ),
+    dash_table.DataTable(
+        id='semi-automatic',
+        columns=[
+            {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
+            {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
+            {'name': 'Event', 'id': 'Event'+ id_str, 'type': 'numeric', 'hideable': True},
+            {'name': 'Event_text', 'id': 'event' + id_str, 'type': 'text', 'hideable': True},
+            {'name': 'Risk', 'id': 'labels' + id_str, 'type': 'numeric', 'editable': True},
+            {'name': 'Context', 'id': 'Context'+ id_str, 'type': 'text'}
+        ],
+        data=df.to_dict('records'),
+        filter_action='native',
+        style_data={
+            'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+        page_size=10),
+    # https://dash.plotly.com/datatable/filtering
     # dash_table.DataTable(
-    #     id='Semi-automatic Analysis',
-    #     columns = [
-    #         {'name': 'timestamp', 'id':'timestamp', 'type': 'any'},
-    #         {'name': 'machine', 'id':'machine', 'type': 'any'},
-    #         {'name': 'Event', 'id':'Event', 'type': 'numeric'},
-    #         {'name': 'label', 'id':'label', 'type': 'numeric'},
-    #         {'name': 'Context', 'id':'Context', 'type': 'any'}
-    #         ],
+    #     id='Context information',
+    #     columns=[
+    #         {'name': 'Event', 'id': 'Event_ci', 'type': 'numeric', 'hideable': True},
+    #         {'name': 'Event_text', 'id': 'event_ci', 'type': 'text', 'hideable': True},
+    #         {'name': 'Confident', 'id': 'Context_ci', 'type': 'text'}
+    #     ],
     #     data=df.to_dict('records'),
     #     filter_action='native',
     #     style_data={
@@ -26,8 +51,6 @@ layout = html.Div([
     #         'overflow': 'hidden',
     #         'textOverflow': 'ellipsis',
     #     },
-    #     page_size = 10)
-    # https://dash.plotly.com/datatable/filtering
-
+    #     page_size=10)
 
 ], style=style.content_style)
