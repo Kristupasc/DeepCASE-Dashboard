f5a0f3fceb6638e09cf3ddf86ac3f1002831ade5
mooon53 <katherinrad2409@gmail.com>
2024-03-20 13:38:32 +0100

    fixed import in automated.p

--- Dashboard/app/main/pages/automated.py
+++ Dashboard/app/main/pages/automated.py
@@ -1,10 +1,8 @@
 import dash
 from dash import html, dash_table, dcc, callback, Output, Input
 import pandas as pd
-import Dashboard.app.main.recources.loaddata as load
 import Dashboard.app.main.recources.style as style
 dash.register_page(__name__, path="/semi-automatic", name="Semi-automatic", title="Semi-automatic", order=2)
-from Dashboard.app.main.pagescallback.automated import *
 
 
 
