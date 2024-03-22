9493766ae7059ab652172259de9634d4c5661321
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-03-22 14:35:05 +0100

    Copy paste, semi

--- Dashboard/app/main/pages/dashboard.py
+++ Dashboard/app/main/pages/dashboard.py
@@ -10,28 +10,77 @@ import Dashboard.app.main.recources.style as style
 # upload through database
 # make it cluster on dashboard
 
-GRAPH_INTERVAL = os.environ.get("GRAPH_INTERVAL", 5000)
-
 dash.register_page(__name__, path="/", name="Dashboard", title="Dashboard", order=0)
-
-# Define color scheme
-colors = {
-    "background": "#FFFFFF",  # white background
-    "text": "#000000",  # black text for visibility
-    "Risk Label": {
-        "Info": "#45B6FE",  # blue
-        "Low": "#FFD700",  # gold
-        "Medium": "#FF8C00",  # darkorange
-        "High": "#FF4500",  # orangered
-        "Attack": "#DC143C",  # crimson
-        "Suspicious": "#800080",  # purple
-        "Unlabeled": "#808080"  # grey
-    }
-}
-
+import dash
+from dash import html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import Dashboard.app.main.recources.loaddata as load
+import Dashboard.app.main.recources.style as style
+from Dashboard.app.main.pagescallback.automated import *
+########################################################################
+#   Dash objects page(Makes use of the callback addition)    #
+########################################################################
 
 layout = html.Div([
-    html.H1('Dashboard'),
-], style=style.content_style)
+    html.H1('Dash'),
+    html.H2('cluster name unknown', id='cluster name' + id_str),
+    # A signal to update the dropdown menu regularly
+    dcc.Interval(
+        id='interval' + id_str,
+        interval=60 * 1000,  # in milliseconds
+        n_intervals=0
+    ),
+    # drop down menu to select cluster
+    dcc.Dropdown(
+            id="filter_dropdown"+ id_str,
+            options=[{"label": i[1], "value": i[0]} for i in set_cluster],
+            placeholder="-Select a Cluster-",
+            multi=False,
+            value=list([i[0] for i in set_cluster])
+        ),
 
+    # data table to display the cluster
+    dash_table.DataTable(
+        id='dashboard',
+        columns=[
+            {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
+            {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
+            {'name': 'Event', 'id': 'id_cluster'+id_str, 'type': 'numeric', 'hideable': True},
+            {'name': 'Event_text', 'id': 'name' + id_str, 'type': 'text', 'hideable': True},
+            {'name': 'Risk', 'id': 'risk_label' + id_str, 'type': 'numeric', 'editable': True},
+        ],
+        data=df.to_dict('records'),
+        filter_action='native',
+        row_selectable="single",
+        style_data={
+            'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+        page_size=10),
+    html.H2('Context of the selected event', id='sequence name' + cid_str),
+    # Table to show the context of a sequence
+    dash_table.DataTable(
+        id='Context information'+cid_str,
+        columns=[
+            {'name': 'Position', 'id': 'event_position'+cid_str, 'type': 'numeric', 'hideable': True},
+            {'name': 'event', 'id': 'event'+cid_str, 'type': 'text', 'hideable': True},
+            {'name': 'Event_name', 'id': 'name'+cid_str, 'type': 'text', 'hideable': True},
+            {'name': 'Attention', 'id': 'attention'+cid_str, 'type': 'text'}
+        ],
+        data=df.to_dict('records'),
+        filter_action='native',
 
+        style_data={
+            'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+        page_size=10),
+    # Objects to store intermediate values, selected by the above table.
+dcc.Store(id='selected cluster'+ id_str),
+dcc.Store(id='selected row'+ id_str)
+
+],
+    # dcc.Store stores the intermediate value
+    style=style.content_style)
---
+++ Dashboard/app/main/pagescallback/dashboard.py
@@ -0,0 +1,119 @@
+from io import StringIO
+
+import dash
+from dash import html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+from dash.exceptions import PreventUpdate
+
+import Dashboard.app.main.recources.loaddata as load
+########################################################################
+#   Dashboard callback (All ids need to match 100%)               #
+########################################################################
+#suffix for all the ids that might be the same.
+id_str = "_da"
+cid_str = "_cida"
+cluster = 0
+df = load.formatSequenceCluster(0, id_str)
+set_cluster = load.possible_clusters()
+@callback(
+    Output('selected cluster' + id_str, "data"),
+    Input("filter_dropdown"+id_str, "value")
+)
+def store_selected_cluster(state):
+    """
+    This methode stores the variable of the dropdown menu.
+    Parameters
+    ----------
+    state: The variable that contains the value.
+
+    Returns
+    -------
+    Return: either PreventUpdate signal
+    or the state to store.
+
+    """
+    if isinstance(state, int):
+        return state
+    raise PreventUpdate
+@callback(
+    Output("dashboard", "data"),
+    Input('selected cluster' + id_str, "data")
+)
+def update_table_cluster(state):
+    """
+    This methode returns a data frame for the cluster table
+    Parameters
+    ----------
+    state is the value that is stored from the selected
+    state.
+
+    Returns
+    -------
+    The dataframe necessary for the cluster.
+
+    """
+    if isinstance(state, int):
+        dff = load.formatSequenceCluster(state, id_str)
+        return dff.to_dict("records")
+    raise PreventUpdate
+
+@callback(
+    Output('selected row' + id_str, "data"),
+    Input("dashboard", 'selected_rows')
+)
+def store_context_row(state):
+    """
+    This methode stores the selected row.
+    Parameters
+    ----------
+    :parameter: state which row is selected.
+
+    Returns
+    -------
+    The value that needs to be stored.
+
+    """
+    if state is not None:
+        if len(state)>0:
+            if isinstance(state[0], int):
+                return state[0]
+    raise PreventUpdate
+@callback(
+    Output('Context information'+cid_str, "data"),
+    Input('selected row' + id_str, "data"),
+    Input('selected cluster' + id_str,"data")
+)
+def display_context(row, cluster):
+    """
+    This makes the context visible
+    :param row: the row that is selected
+    :param cluster: the cluster that is selected
+    :return: the context frame.
+    """
+    if isinstance(row, int) and isinstance(cluster, int):
+        df = load.formatContext(cluster, row, cid_str)
+        return df.to_dict("records")
+    raise PreventUpdate
+
+@callback(
+    Output("filter_dropdown"+ id_str, 'options'),
+    Input('interval'+ id_str, 'n_intervals')
+)
+def update_options_dropdown(n):
+    return [{"label": i[1], "value": i[0]} for i in load.possible_clusters()]
+@callback(
+    Output("filter_dropdown"+ id_str, 'value'),
+    Input('interval'+ id_str, 'n_intervals')
+)
+def update_options_dropdown(n):
+    return list([i[0] for i in load.possible_clusters()])
+@callback(
+    Output('cluster name' + id_str, 'children'),
+    Input('selected cluster' + id_str,"data")
+)
+def get_name_cluster(data):
+    if isinstance(data, int):
+        k =  load.possible_clusters()
+        for z in k:
+            if z[0] == data:
+                return z[1]
