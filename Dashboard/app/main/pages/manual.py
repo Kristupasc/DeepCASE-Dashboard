088006dd3f851a6d9313b87826e20405dc00c2a1
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-03-22 15:40:04 +0100

    Fix manual

--- Dashboard/app/main/pages/manual.py
+++ Dashboard/app/main/pages/manual.py
@@ -2,21 +2,24 @@ import dash
 from dash import html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import Dashboard.app.main.recources.style as style
-dash.register_page(__name__, path="/manual-analysis", name="Manual Analysis", title="Manual Analysis", order=1)
-
-import dash
 from dash import html, dash_table, dcc, callback, Output, Input
-import pandas as pd
-import Dashboard.app.main.recources.loaddata as load
 import Dashboard.app.main.recources.style as style
 from Dashboard.app.main.pagescallback.manual import *
 from Dashboard.app.main.pagescallback.manual1 import *
+
+import dash
+
+import pandas as pd
+import Dashboard.app.main.recources.loaddata as load
+dash.register_page(__name__, path="/manual-analysis", name="Manual Analysis", title="Manual Analysis", order=1)
+
+
 ########################################################################
 #   Manual objects page(Makes use of the callback addition)    #
 ########################################################################
 
 layout = html.Div([
-    html.H1('Semi-automatic'),
+    html.H1('Manual'),
     html.H2('cluster name unknown', id='cluster name' + id_str),
     # A signal to update the dropdown menu regularly
     dcc.Interval(
@@ -26,20 +29,20 @@ layout = html.Div([
     ),
     # drop down menu to select cluster
     dcc.Dropdown(
-            id="filter_dropdown"+ id_str,
-            options=[{"label": i[1], "value": i[0]} for i in set_cluster],
-            placeholder="-Select a Cluster-",
-            multi=False,
-            value=list([i[0] for i in set_cluster])
-        ),
+        id="filter_dropdown" + id_str,
+        options=[{"label": i[1], "value": i[0]} for i in set_cluster],
+        placeholder="-Select a Cluster-",
+        multi=False,
+        value=list([i[0] for i in set_cluster])
+    ),
 
     # data table to display the cluster
     dash_table.DataTable(
-        id='semi-automatic',
+        id='manual',
         columns=[
             {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
             {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
-            {'name': 'Event', 'id': 'id_cluster'+id_str, 'type': 'numeric', 'hideable': True},
+            {'name': 'Event', 'id': 'id_cluster' + id_str, 'type': 'numeric', 'hideable': True},
             {'name': 'Event_text', 'id': 'name' + id_str, 'type': 'text', 'hideable': True},
             {'name': 'Risk', 'id': 'risk_label' + id_str, 'type': 'numeric', 'editable': True},
         ],
@@ -55,12 +58,12 @@ layout = html.Div([
     html.H2('Context of the selected event', id='sequence name' + cid_str),
     # Table to show the context of a sequence
     dash_table.DataTable(
-        id='Context information'+cid_str,
+        id='Context information' + cid_str,
         columns=[
-            {'name': 'Position', 'id': 'event_position'+cid_str, 'type': 'numeric', 'hideable': True},
-            {'name': 'event', 'id': 'event'+cid_str, 'type': 'text', 'hideable': True},
-            {'name': 'Event_name', 'id': 'name'+cid_str, 'type': 'text', 'hideable': True},
-            {'name': 'Attention', 'id': 'attention'+cid_str, 'type': 'text'}
+            {'name': 'Position', 'id': 'event_position' + cid_str, 'type': 'numeric', 'hideable': True},
+            {'name': 'event', 'id': 'event' + cid_str, 'type': 'text', 'hideable': True},
+            {'name': 'Event_name', 'id': 'name' + cid_str, 'type': 'text', 'hideable': True},
+            {'name': 'Attention', 'id': 'attention' + cid_str, 'type': 'text'}
         ],
         data=df.to_dict('records'),
         filter_action='native',
@@ -72,8 +75,8 @@ layout = html.Div([
         },
         page_size=10),
     # Objects to store intermediate values, selected by the above table.
-dcc.Store(id='selected cluster'+ id_str),
-dcc.Store(id='selected row'+ id_str)
+    dcc.Store(id='selected cluster' + id_str),
+    dcc.Store(id='selected row' + id_str)
 
 ],
     # dcc.Store stores the intermediate value
