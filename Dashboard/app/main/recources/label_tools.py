246ad471f667d3644eb0862ef26c2c6920e6ddb1
s2689669 <k.cepelis@student.utwente.nl>
2024-03-25 14:52:31 +0100

    format changes

--- Dashboard/app/main/recources/label_tools.py
+++ Dashboard/app/main/recources/label_tools.py
@@ -1,23 +1,21 @@
-
-
-
 def get_colors():
     return {
-    "background": "#FFFFFF",  # white background
-    "text": "#000000",  # black text for visibility
-    "Risk Label": {
-        "Info": "#45B6FE",  # blue
-        "Low": "#FFD700",  # gold
-        "Medium": "#FF8C00",  # darkorange
-        "High": "#FF4500",  # orangered
-        "Attack": "#DC143C",  # crimson
-        "Suspicious": "#800080",  # purple
-        "Unlabeled": "#808080"  # grey
+        "background": "#FFFFFF",  # white background
+        "text": "#000000",  # black text for visibility
+        "Risk Label": {
+            "Info": "#45B6FE",  # blue
+            "Low": "#FFD700",  # gold
+            "Medium": "#FF8C00",  # darkorange
+            "High": "#FF4500",  # orangered
+            "Attack": "#DC143C",  # crimson
+            "Suspicious": "#800080",  # purple
+            "Unlabeled": "#808080"  # grey
+        }
     }
-}
+
 
 def choose_risk(weight):
-    #TODO: change it to accept a different format than 0-10
+    # TODO: change it to accept a different format than 0-10
     if weight == 0:
         return "Unlabeled"
     elif weight < 1:
