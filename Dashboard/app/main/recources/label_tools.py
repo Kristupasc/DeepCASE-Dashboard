f53ade01b48764308961b9f2571a0648cf9918c6
s2689669 <k.cepelis@student.utwente.nl>
2024-03-25 14:51:18 +0100

    Added colors and weight function

---
+++ Dashboard/app/main/recources/label_tools.py
@@ -0,0 +1,34 @@
+
+
+
+def get_colors():
+    return {
+    "background": "#FFFFFF",  # white background
+    "text": "#000000",  # black text for visibility
+    "Risk Label": {
+        "Info": "#45B6FE",  # blue
+        "Low": "#FFD700",  # gold
+        "Medium": "#FF8C00",  # darkorange
+        "High": "#FF4500",  # orangered
+        "Attack": "#DC143C",  # crimson
+        "Suspicious": "#800080",  # purple
+        "Unlabeled": "#808080"  # grey
+    }
+}
+
+def choose_risk(weight):
+    #TODO: change it to accept a different format than 0-10
+    if weight == 0:
+        return "Unlabeled"
+    elif weight < 1:
+        return "Info"
+    elif weight < 3:
+        return "Low"
+    elif weight < 5:
+        return "Medium"
+    elif weight < 7:
+        return "High"
+    elif weight < 9:
+        return "Suspicious"
+    else:
+        return "Attack"
