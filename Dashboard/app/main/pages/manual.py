1d0d89c4e137a36d72e8cdb79b37e66980aac429
bobicez <bobicemj@gmail.com>
2024-03-30 20:31:59 +0100

    Typos/text changes & attempt to make table rows change color upon selection

--- Dashboard/app/main/pages/manual.py
+++ Dashboard/app/main/pages/manual.py
@@ -18,13 +18,13 @@ dash.register_page(__name__, path="/manual-analysis", name="Manual Analysis", ti
 ########################################################################
 
 layout = html.Div([
-    html.H1('Manual'),
+    html.H1('Manual analysis'),
     # Change name of cluster and display
     dcc.Textarea(id='cluster name'+id_str, value='Cluster name unknown'),
     html.Button('Change cluster name', id='change cluster name', n_clicks=0),
     html.P(id="set label cluster"+id_str),
     # Get new cluster
-    html.Button('Chose next cluster', id='random' + id_str, n_clicks=0),
+    html.Button('Choose next cluster', id='random' + id_str, n_clicks=0),
     # drop down menu to select cluster
     dcc.Dropdown(
         id="filter_dropdown" + id_str,
@@ -54,9 +54,9 @@ layout = html.Div([
         },
         page_size=10),
     ################## Editable risk values
-    html.Button('Chose next sequence', id='random' + qid_str, n_clicks=0),
+    html.Button('Choose next sequence', id='random' + qid_str, n_clicks=0),
     html.H3(id='doneCluster' + qid_str),
-    html.H3("Event to be labeled."),
+    html.H3("Event to be labeled:"),
     dash_table.DataTable( id='chosen sequence manual',
         columns=[
             {'name': 'Date', 'id': 'timestamp' + qid_str, 'type': 'text'},
@@ -78,14 +78,15 @@ layout = html.Div([
 
 
 
-    ################## Display context
+    ## Selected sequence context
+    html.Div(className='manual_context', children=[
     html.H2('Context of the selected sequence', id='sequence name' + cid_str),
     # Table to show the context of a sequence
     dash_table.DataTable(
         id='Context information' + cid_str,
         columns=[
             {'name': 'Position(top old, bottom newest)', 'id': 'event_position' + cid_str, 'type': 'numeric', 'hideable': True},
-            {'name': 'event', 'id': 'event' + cid_str, 'type': 'text', 'hideable': True},
+            {'name': 'Event', 'id': 'event' + cid_str, 'type': 'text', 'hideable': True},
             {'name': 'Event_name', 'id': 'name' + cid_str, 'type': 'text', 'hideable': True},
             {'name': 'Attention', 'id': 'attention' + cid_str, 'type': 'text'}
         ],
@@ -102,6 +103,8 @@ layout = html.Div([
     dcc.Store(id='selected cluster' + id_str),
     dcc.Store(id='selected row' + id_str)
 
-],
+    ],)
+    ],
     # dcc.Store stores the intermediate value
-    style=style.content_style)
+    style=style.content_style
+)
