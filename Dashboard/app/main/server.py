bd21404dbbb75a7685e64c7f8b785906610adbe7
mooon53 <katherinrad2409@gmail.com>
2024-04-04 15:13:19 +0200

    Fixed database creation error

--- Dashboard/app/main/server.py
+++ Dashboard/app/main/server.py
@@ -2,9 +2,10 @@ from dash import Dash
 from Dashboard.processing.process_split import ProcessorAccessObject
 from Dashboard.data.dao.database import Database
 
-pao = ProcessorAccessObject()
+
 db = Database()
 db.create_tables()
+pao = ProcessorAccessObject()
 app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True, meta_tags=[{'name': 'viewport',
                       'content': 'width=device-width, initial-scale=1.0'}])
 
