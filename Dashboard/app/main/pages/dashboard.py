9d9190fc57890c00a00cb46fd20591fffad2c1f0
bobicez <bobicemj@gmail.com>
2024-04-07 17:07:58 +0200

    -Merged development & frontendcss & removed many conflicts & redundancies -Fixed some minor styling

--- Dashboard/app/main/pages/dashboard.py
+++ Dashboard/app/main/pages/dashboard.py
@@ -1,5 +1,8 @@
 import Dashboard.app.main.recources.style as style
 from Dashboard.app.main.pagescallback.dashboard import *
+# from Dashboard.app.main.pagescallback.display_sequence import *
+from dash import html, dcc, dash_table
+import dash
 
 dash.register_page(__name__, path="/dashboard", name="Dashboard", title="Dashboard", order=0)
 
@@ -14,35 +17,85 @@ layout = html.Div(className='content', children=[
         html.Div(className='subcontent', children=[
             html.H2('cluster name unknown', id='cluster name' + id_str),
             # drop down menu to select cluster
-            dcc.Dropdown(
-                id="filter_dropdown" + id_str,
-                options=update_options_dropdown(None),
-                value=update_values_dropdown(None),
-                placeholder="-Select a Cluster-",
-                multi=False,
-            ),
+            # dcc.Dropdown(
+            #     id="filter_dropdown" + id_str,
+            #     options=update_options_dropdown(None),
+            #     value=update_values_dropdown(None),
+            #     placeholder="-Select a Cluster-",
+            #     multi=False,
+            # ),
 
             # data table to display the cluster
-            dash_table.DataTable(
-                id='dashboard',
-                columns=[
-                    {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
-                    {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
-                    {'name': 'Event', 'id': 'id_event' + id_str, 'type': 'numeric', 'hideable': True},
-                    {'name': 'Event_text', 'id': 'name' + id_str, 'type': 'text', 'hideable': True},
-                    {'name': 'Risk', 'id': 'risk_label' + id_str, 'type': 'numeric'},
-                ],
-                # data=df.to_dict('records'),
-                filter_action='native',
-                row_selectable="single",
-                style_data={
-                    'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
-                    'overflow': 'hidden',
-                    'textOverflow': 'ellipsis',
-                },
-                page_size=10)
-        ]),
+            # dash_table.DataTable(
+            #     id='dashboard',
+            #     columns=[
+            #         {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
+            #         {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
+            #         {'name': 'Event', 'id': 'id_event' + id_str, 'type': 'numeric', 'hideable': True},
+            #         {'name': 'Event_text', 'id': 'name' + id_str, 'type': 'text', 'hideable': True},
+            #         {'name': 'Risk', 'id': 'risk_label' + id_str, 'type': 'numeric'},
+            #     ],
+            #     # data=df.to_dict('records'),
+            #     filter_action='native',
+            #     row_selectable="single",
+            #     style_data={
+            #         'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+            #         'overflow': 'hidden',
+            #         'textOverflow': 'ellipsis',
+            #     },
+            #     page_size=10)
+        ],),
+
         html.Div(className='subcontent', children=[
+        # Change name of file and display
+        dcc.Textarea(id='filename' + id_str, value='File not selected'),
+        html.Button('Change filename', id='change filename' + id_str, n_clicks=0),
+        dcc.Dropdown(
+            id="filename_dropdown" + id_str,
+            placeholder="-Select a file-",
+            multi=False,
+            clearable=False,
+        ),
+            html.Div(
+                style={'display': 'flex', 'alignItems': 'center'},
+                children=[
+                    html.H2('Cluster name unknown', id='cluster-name' + id_str),
+                    html.H3('(Security label cluster:', style={'marginLeft': '10px'}),
+                    html.H3('8) "To be fixed/removed?"', id="display-risk-cluster" + id_str,
+                            style={'marginLeft': '10px'}),
+                ]
+            ),
+
+            # drop down menu to select cluster
+        dcc.Dropdown(
+            id="filter_dropdown" + id_str,
+            options=update_options_dropdown(None),
+            value=update_values_dropdown(None),
+            placeholder="-Select a Cluster-",
+            clearable=False,
+            multi=False,
+        ),
+              # # data table to display the cluster
+        # dash_table.DataTable(
+        #     id='dashboard',
+        #     columns=[
+        #         {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
+        #         {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
+        #         {'name': 'Event', 'id': 'id_event' + id_str, 'type': 'numeric', 'hideable': True},
+        #         {'name': 'Event_text', 'id': 'name' + id_str, 'type': 'text', 'hideable': True},
+        #         {'name': 'Risk', 'id': 'risk_label' + id_str, 'type': 'numeric'},
+        #     ],
+        #     # data=df.to_dict('records'),
+        #     filter_action='native',
+        #     row_selectable="single",
+        #     style_data={
+        #         'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+        #         'overflow': 'hidden',
+        #         'textOverflow': 'ellipsis',
+        #     },
+        #     page_size=10),
+
+        html.Div(className='semiauto_context', children=[
             html.H2('Context of the selected sequence', id='sequence name' + cid_str),
 
             # Table to show the context of a sequence
@@ -86,23 +139,21 @@ layout = html.Div(className='content', children=[
         ),
 
         # Graph to display the scatter plot
-        html.Div(children=
-        [
-            html.H2("All Sequences in Cluster", className="graph__title"),
-            dcc.Graph(id="scatter-plot"),
-            dcc.Interval(
-                id="scatter-update",
-                interval=int(5000),
-                n_intervals=0,
-            ),
-        ],
-        ), ]
+        html.Div(children=[html.H2("All Sequences in Cluster", className="graph__title"),
+                           dcc.Graph(id="scatter-plot"),
+                           dcc.Interval(
+                               id="scatter-update",
+                               interval=int(5000),
+                               n_intervals=0,
+                           ),
+                           ],
+                 ), ]
              ),
 
     # Objects to store intermediate values, selected by the above table.
     dcc.Store(id='selected cluster' + id_str),
     dcc.Store(id='selected row' + id_str),
 
-],
+])
     # dcc.Store stores the intermediate value
-)
+])
--- Dashboard/app/main/pages/manual.py
+++ Dashboard/app/main/pages/manual.py
@@ -1,16 +1,6 @@
-import dash
-from dash import html, dash_table, dcc, callback, Output, Input
-import pandas as pd
-import Dashboard.app.main.recources.style as style
-from dash import html, dash_table, dcc, callback, Output, Input
 import Dashboard.app.main.recources.style as style
 from Dashboard.app.main.pagescallback.manual import *
-
-import dash
-
-import pandas as pd
-import Dashboard.app.main.recources.loaddata as load
-
+import dash_mantine_components as dmc
 dash.register_page(__name__, path="/manual-analysis", name="Manual Analysis", title="Manual Analysis", order=1)
 
 ########################################################################
@@ -21,23 +11,84 @@ layout = html.Div(className='content', children=[
     html.H1('Manual analysis'),
     html.Div(className='subcontent', children=[
         # Change name of cluster and display
-        dcc.Textarea(id='cluster name' + id_str, value='Cluster name unknown'),
-        html.Button('Change cluster name', id='change cluster name', n_clicks=0),
-        html.P(id="set label cluster" + id_str),
+        # dcc.Textarea(id='cluster name' + id_str, value='Cluster name unknown'),
+        # html.Button('Change cluster name', id='change cluster name', n_clicks=0),
+    html.P(id="set label cluster" + id_str),
         # Get new cluster
-        html.Button('Choose next cluster', id='random' + id_str, n_clicks=0),
+        # html.Button('Choose next cluster', id='random' + id_str, n_clicks=0),
         # drop down menu to select cluster
-        dcc.Dropdown(
-            id="filter_dropdown" + id_str,
-            # options=[{"label": i[1], "value": i[0]} for i in set_cluster],
-            placeholder="-Select a Cluster-",
-            multi=False,
-            # value=list([i[0] for i in set_cluster])
-        ),
+        # dcc.Dropdown(
+        #     id="filter_dropdown" + id_str,
+        #     # options=[{"label": i[1], "value": i[0]} for i in set_cluster],
+        #     placeholder="-Select a Cluster-",
+        #     multi=False,
+        #     # value=list([i[0] for i in set_cluster])
+        # ),
+    html.Div([
+    # html.H1('Manual analysis'),
+    # Start automatic analysis.
+    html.Button('Start semi-automatic analysis', id='start automatic', n_clicks=0),
+    # Change name of file and display
+    dcc.Textarea(id='filename' + id_str, value='File not selected'),
+    html.Button('Change filename', id='change filename'+id_str, n_clicks=0),
+    dcc.Dropdown(
+        id="filename_dropdown" + id_str,
+        placeholder="-Select a file-",
+        multi=False,
+        clearable=False,
+    ),
+    # Change name of cluster and display
+    # dcc.Textarea(id='cluster name' + id_str, value='Cluster name unknown'),
+    html.Div([
+        html.H3('Security label cluster: '),
+        html.H3(' Fix this & add correct value', id="display risk cluster"+id_str),
+    ], style= {'display': 'flex'}),
+    dcc.Textarea(id='cluster name - needs fixing apparently' + id_str, value='Cluster name unknown'),
+    html.Button('Change cluster name', id='change cluster name', n_clicks=0),
+    # Get new cluster
+    html.Button('Choose random cluster', id='random' + id_str, n_clicks=0),
+    html.Button('Choose random sequence', id='random' + qid_str, n_clicks=0),
+    # drop down menu to select cluster
+    dcc.Dropdown(
+        id="filter_dropdown" + id_str,
+        # options=[{"label": i[1], "value": i[0]} for i in set_cluster],
+        placeholder="-Select a Cluster-",
+        clearable=False,
+        multi=False,
+        # value=list([i[0] for i in set_cluster])
+    ),
 
         # data table to display the cluster
+        # dash_table.DataTable(
+        #     id='manual',
+        #     columns=[
+        #         {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
+        #         {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
+        #         {'name': 'Event', 'id': 'id_event' + id_str, 'type': 'numeric', 'hideable': True},
+        #         {'name': 'Event_text', 'id': 'name' + id_str, 'type': 'text', 'hideable': True},
+        #         {'name': 'Risk', 'id': 'risk_label' + id_str, 'type': 'numeric', 'editable': True},
+        #     ],
+        #     # data=df.to_dict('records'),
+        #     filter_action='native',
+        #     row_selectable="single",
+        #     style_data={
+        #         'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+        #         'overflow': 'hidden',
+        #         'textOverflow': 'ellipsis',
+        #     },
+        #     page_size=10),
+        ################## Editable risk values
+        # html.Button('Choose next sequence', id='random' + qid_str, n_clicks=0),
+        html.H3(id='doneCluster' + qid_str),
+        # html.Button('Submit change', id='submit' + qid_str, n_clicks=0),
+        html.H3(id="successful" + qid_str),
+    ]),
+    # data table to display the cluster
+    html.Div(className='sequences_table',children = [
         dash_table.DataTable(
             id='manual',
+            # renamable=True,
+            editable=True,
             columns=[
                 {'name': 'Date', 'id': 'timestamp' + id_str, 'type': 'text'},
                 {'name': 'Source', 'id': 'machine' + id_str, 'type': 'text'},
@@ -55,11 +106,12 @@ layout = html.Div(className='content', children=[
             },
             page_size=10),
         ################## Editable risk values
-        html.Button('Choose next sequence', id='random' + qid_str, n_clicks=0),
+        # html.Button('Choose random sequence', id='random' + qid_str, n_clicks=0),
         html.H3(id='doneCluster' + qid_str),
         html.Button('Submit change', id='submit' + qid_str, n_clicks=0),
         html.H3(id="successful" + qid_str),
-    ]),
+    ],),
+    # style={"display": "flex"}),
 
     ## Selected sequence context
     html.Div(className='subcontent', children=[
@@ -87,6 +139,11 @@ layout = html.Div(className='content', children=[
         dcc.Store(id='selected cluster' + id_str),
         dcc.Store(id='selected row' + id_str)
 
-    ], )
-    ]
+    ], ),
+    dmc.Modal( title="Yeeeeeeeeaaaaaaaa", id = "modal_set_cluster"+id_str),
+    dmc.Modal( title="Noooooooo", id = "modal_set_risk"+id_str),
+],
+    # dcc.Store stores the intermediate value
+    # style=style.content_style
 )
+])
