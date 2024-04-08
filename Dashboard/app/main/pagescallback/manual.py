5e3bb5f7b76e3ebb6bb3c3d8f859ff32996ed25a
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-04-08 14:55:45 +0200

    Error handling.

--- Dashboard/app/main/pagescallback/manual.py
+++ Dashboard/app/main/pagescallback/manual.py
@@ -222,8 +222,11 @@ def run_deepcase(n_clicks, opened):
 
     """
     if 'start automatic' == ctx.triggered_id:
-        pao = load.start_automatic()
-        return not opened, pao.status.name
+        try:
+            pao = load.start_automatic()
+            return not opened, pao.get_status()
+        except:
+            return not opened, "File not selected."
     return opened, "Not pressed button"
 ########################################################################################
 # Find the risk value of cluster and display
