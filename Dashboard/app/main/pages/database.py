cd843561d4a7e248afb55317d57d425dff9fca93
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-04-18 15:02:58 +0200

    Merge file, to keep all new callbacks working.

--- Dashboard/app/main/pages/database.py
+++ Dashboard/app/main/pages/database.py
@@ -1,118 +1,64 @@
-import base64
-from datetime import datetime
-from threading import Thread
-
 import dash
-import pandas as pd
-from dash import html, dcc, callback
-from Dashboard.data.upload import create_database
-import io
-from dash.dependencies import Input, Output, State
-import Dashboard.app.main.recources.style as style
-from Dashboard.processing.process_split import ProcessorAccessObject
-from Dashboard.data.dao.dao import DAO
+from Dashboard.app.main.pagescallback.database import *
+from dash import html, dcc, dash_table
+import dash_mantine_components as dmc
 
 dash.register_page(__name__, path="/database", name="Database", title="Database", order=3)
-dao = DAO()
 
-layout = html.Div(className = 'content', children=[
+layout = html.Div(className='content', children=[
     html.H1('Database'),
     html.Div(className='subcontent', children=[
         html.Div(className='top-bar', children=[
-        dcc.Upload(
-            id='upload-data',
-            children=html.Button("Upload File"),
-            # Allow multiple files to be uploaded
-            multiple=True
-        ),
-        dcc.Dropdown(
-            id="file-dropdown",
-            placeholder="-Select a file-",
-            multi=False,
-            clearable=False,
-        ),
+            html.Div(id='parent-upload-data' + id_str,
+                     children=[
+                         dcc.Upload(
+                             id='upload-data' + id_str,
+                             children=html.Button("Upload File"),
+                             # Allow multiple files to be uploaded
+                             multiple=True
+                         ), ]
+                     ),
+
+            dcc.Dropdown(
+                id="file-dropdown" + id_str,
+                placeholder="-Select a file-",
+                multi=False,
+                clearable=False,
+            ),
         ]),
-    html.Div([
-            dcc.Location(id='url', refresh=False),
-            html.Div(id='output-data-upload')
+        html.Div([
+            dcc.Location(id='url' + id_str, refresh=False),
+            html.Button('Start Security Analysis', id='start_deepcase_btn' + id_str),
+            dash_table.DataTable(
+                id="uploaded data" + id_str,
+                columns=[
+                    {'name': 'id_event', 'id': 'id_event', 'type': 'text'},
+                    {'name': 'filename', 'id': 'filename', 'type': 'text'},
+                    {'name': 'timestamp', 'id': 'timestamp', 'type': 'text'},
+                    {'name': 'machine', 'id': 'machine', 'type': 'text'},
+                    {'name': 'event', 'id': 'event', 'type': 'text'},
+                    {'name': 'label', 'id': 'label', 'type': 'text'}
+                ],
+                filter_action='native',
+                style_data={
+                    'width': 'normal', 'minWidth': 'normal', 'maxWidth': 'normal',
+                    'overflow': 'hidden',
+                    'textOverflow': 'ellipsis',
+                    'table-layout': 'auto',
+                    'font-size': '11px',
+                },
+                page_size=50
+            ),
         ])
-    ])
+    ]),
+    dmc.Modal(title="Yeeeeeeeeaaaaaaaa", id="feedback_deepcase" + id_str),
+    dmc.Modal(title="DeepCASE is running.\n\n This might take 20 minutes.\n\n Enjoy a cup of tea in the meantime.", id="feedback_start_deepcase" + id_str,
+              children=[dcc.Loading(
+                      id="loading start deepcase",
+                      children=[html.Div([html.Div(id="loading output start deepcase")])],
+                      type="circle",
+                      fullscreen=False
+                  )]),
+    dmc.Modal(title="Yeeeeeeeeaaaaaaaa", id="feedback_save_file" + id_str),
+    dmc.Modal(title="noo", id="feedback_switch" + id_str),
 ])
-
-
-# Called when a user uploads a new file
-@callback(Output('output-data-upload', 'children', allow_duplicate=True),
-          Input('upload-data', 'contents'),
-          State('upload-data', 'filename'),
-          State('upload-data', 'last_modified'),
-          prevent_initial_call=True
-          )
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            create_database.parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-
-
-# Called when at the start => retrieves the events table
-@callback(Output('output-data-upload', 'children'),
-          Input('url', 'pathname'))
-def display_table(pathname):
-    table = create_database.displayDataFile()
-    return table
-
-
-@callback(
-    Output('output-data-upload', 'children', allow_duplicate=True),
-    Input('file-dropdown', 'value'),
-    Input('url', 'pathname'),
-    prevent_initial_call=True)
-def update_selected_file(value, pathname):
-    dao.switch_current_file(value)
-    return display_table(pathname=pathname)
-
-
-@callback(
-    Output("file-dropdown", "options"),
-    Input('output-data-upload', 'children'),
-    Input('file-dropdown', 'value')
-)
-def update_options(children, value):
-    return dao.get_filenames().values.flatten().tolist()
-
-
-@callback(Output('deepcase-status-display', 'children', allow_duplicate=True),
-          Input('start_deepcase_btn', 'n_clicks'),
-          prevent_initial_call=True)
-def run_deepcase(n_clicks):
-    if n_clicks and n_clicks > 0:
-        pao = ProcessorAccessObject()
-        thread = Thread(target=pao.run_DeepCASE())
-        thread.start()
-        return "DeepCASE process is finished. You can review results on Manual Analysis page."
-    return dash.no_update
-
-
-@callback(
-    Output('start_deepcase_btn', 'style'),
-    Input('start_deepcase_btn', 'n_clicks'),
-    prevent_initial_call=True
-
-)
-def hide_button(n_clicks):
-    if n_clicks and n_clicks > 0:
-        return {'display': 'none'}
-    return {}
-
-
-@callback(
-    Output('deepcase-status-display', 'children'),
-    Input('start_deepcase_btn', 'n_clicks')
-)
-def hide_button(n_clicks):
-    if n_clicks and n_clicks > 0:
-        return 'DeepCASE is running. Please do not close this page until the process is finished. It may take several minutes.'
-    return dash.no_update
-
-
