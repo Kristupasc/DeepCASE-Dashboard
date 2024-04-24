040e91b634c8b1b98c4836603eb9fe23f8bd8ab6
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-04-24 12:44:33 +0200

    Test create database, still a fail

--- Dashboard/test/app/main/recources/test_create_database.py
+++ Dashboard/test/app/main/recources/test_create_database.py
@@ -1,14 +1,11 @@
 from unittest import TestCase
-
-from Dashboard.app.main.recources.loaddata import is_file_selected
-from Dashboard.data.dao.dao import DAO
-
+from Dashboard.app.main.recources.create_database import *
+import base64
 
 class Test(TestCase):
-    def setUp(self):
-        dao = DAO()
-        dao.switch_current_file(dao.get_filenames().iloc[0].at["custom_name"])
-        if not is_file_selected:
-            raise Exception("No file present, run first deepcase")
     def test_parse_contents(self):
-        self.fail()
+        file_text = open("C:\\Users\marij\PycharmProjects\M11_3\DeepCASE-Dashboard\Dashboard\\app\main\server.py", 'rb')
+        file_read = file_text.read()
+        file_encode = base64.encodebytes(file_read)
+        parse_contents(file_encode,"test_123", 0.0)
+
