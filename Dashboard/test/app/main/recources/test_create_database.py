aeffaff20f70fde458db37e321f25585d8eae012
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-04-24 12:22:27 +0200

    Testing

--- Dashboard/test/app/main/recources/test_create_database.py
+++ Dashboard/test/app/main/recources/test_create_database.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 
+from Dashboard.app.main.recources.loaddata import is_file_selected
+from Dashboard.data.dao.dao import DAO
+
 
 class Test(TestCase):
+    def setUp(self):
+        dao = DAO()
+        dao.switch_current_file(dao.get_filenames().iloc[0].at["custom_name"])
+        if not is_file_selected:
+            raise Exception("No file present, run first deepcase")
     def test_parse_contents(self):
         self.fail()
