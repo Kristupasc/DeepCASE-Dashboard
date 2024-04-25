4e6ee0028977ed104b4a80d933dfa55075e7b52b
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-04-25 12:02:52 +0200

    Fix depreciate warning

--- Dashboard/test/app/main/recources/test_create_database.py
+++ Dashboard/test/app/main/recources/test_create_database.py
@@ -7,9 +7,9 @@ class Test(TestCase):
         file_text = open("create_database.txt", 'rb')
         file_read = file_text.read().decode()
 
-        self.assertAlmostEquals(parse_contents(file_read,"test_123.csv", 0.0), 'File Uploaded Successfully: ' + "test_123.csv"+'' \
+        self.assertAlmostEqual(parse_contents(file_read,"test_123.csv", 0.0), 'File Uploaded Successfully: ' + "test_123.csv"+'' \
                                                      '\n\n\nPress "Start Security Analysis" button to run DeepCASE.')
 
-        self.assertAlmostEquals(parse_contents(file_read,"test_123.csv", 0.0), 'File Uploaded Successfully: ' + "test_123.csv"+'' \
+        self.assertAlmostEqual(parse_contents(file_read,"test_123.csv", 0.0), 'File Uploaded Successfully: ' + "test_123.csv"+'' \
                                                      '\n\n\nPress "Start Security Analysis" button to run DeepCASE.')
 
