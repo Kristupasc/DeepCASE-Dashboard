af6191fb2616622fcb484332827aa853405aaff9
Kristupasc <k.cepelis@gmail.com>
2024-04-25 15:26:50 +0200

    Changed how we find the markdown file

--- Dashboard/app/main/pages/user_instruction.py
+++ Dashboard/app/main/pages/user_instruction.py
@@ -1,10 +1,20 @@
+import os
 import dash
 from dash import html, dcc
+
 dash.register_page(__name__, path="/user-manual", name="User Manual", title="User Manual", order=0)
 ########################################################################
 #                       User-manual page, from Markdown file.          #
 ########################################################################
-with open('../static/User_Manual_Interface_Usage_Guide.md', 'r') as instruction_file:
+
+# we need to get the location of the markdown file. We use absolute paths to make sure the file is found in docker.
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path to the file using a raw string literal
+file_path = os.path.join(current_dir, r'../../static/User_Manual_Interface_Usage_Guide.md')
+
+with open(file_path, 'r') as instruction_file:
     instructions = instruction_file.read()
     instruction_file.close()
 
@@ -15,4 +25,4 @@ layout = html.Div(className = 'content', children = [
             instructions
         )
     ])
-    ])
+])
